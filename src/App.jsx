46862972c71e6c44f4651f6fd27578f7153b0057
Vo Minh Thien NGUYEN <minhthien.ng4612@gmail.com>
2024-03-11 21:12:11 +0200

    MUI date picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseLine";
 import ToDoList from "./ToDoList.jsx";
 function App() {
   return (
-    <>
+    <Container maxWidth="x1">
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">My Todos</Typography>
+        </Toolbar>
+      </AppBar>
       <ToDoList />
-    </>
+    </Container>
   );
 }
 
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,5 +1,11 @@
 import { useState, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 
@@ -17,6 +23,7 @@ function ToDoList() {
   ]);
   const [todo, setTodo] = useState({ Description: "", Priority: "", Date: "" });
   const [todos, setTodos] = useState([]);
+  const [value] = useState(null);
   const gridRef = useRef();
 
   const addToDo = () => {
@@ -39,35 +46,50 @@ function ToDoList() {
       alert("Select a row first!");
     }
   };
+  const dateChange = (date) => {
+    setTodo({ ...todo, Date: date });
+  };
   return (
     <>
-      <input
-        placeholder="Description"
-        onChange={(e) => setTodo({ ...todo, Description: e.target.value })}
-        value={todo.Description}
-      />
-      <input
-        type="date"
-        placeholder="Date"
-        onChange={(e) => setTodo({ ...todo, Date: e.target.value })}
-        value={todo.Date}
-      />
-      <input
-        placeholder="Priority"
-        onChange={(e) => setTodo({ ...todo, Priority: e.target.value })}
-        value={todo.Priority}
-      />
-      <button onClick={addToDo}>Add</button>
-      <button onClick={deleteToDo}>Delete</button>
-      <div className="ag-theme-material" style={{ width: 700, height: 500 }}>
-        <AgGridReact
-          ref={gridRef}
-          onGridReady={(params) => (gridRef.current = params.api)}
-          rowData={todos}
-          columnDefs={columnDefs}
-          rowSelection="single"
-        />
-      </div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Stack
+          mt={2}
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <TextField
+            placeholder="Description"
+            onChange={(e) => setTodo({ ...todo, Description: e.target.value })}
+            value={todo.Description}
+          />
+          <DatePicker
+            value={value}
+            onChange={date => dateChange(date.toISOString().substring(0,10))}
+          />
+          <TextField
+            placeholder="Priority"
+            onChange={(e) => setTodo({ ...todo, Priority: e.target.value })}
+            value={todo.Priority}
+          />
+          <Button variant="outlined" onClick={addToDo}>
+            Add
+          </Button>
+          <Button variant="outlined" color="error" onClick={deleteToDo}>
+            Delete
+          </Button>
+        </Stack>
+        <div className="ag-theme-material" style={{ width: 700, height: 500 }}>
+          <AgGridReact
+            ref={gridRef}
+            onGridReady={(params) => (gridRef.current = params.api)}
+            rowData={todos}
+            columnDefs={columnDefs}
+            rowSelection="single"
+          />
+        </div>
+      </LocalizationProvider>
     </>
   );
 }
